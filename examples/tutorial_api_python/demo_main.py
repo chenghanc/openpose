42cd5f6d36a4132dd8ce68f3d3184251bc0f25d5
chenghanc <chenghan.chung@rwth-aachen.de>
2022-04-27 10:29:20 +0800

    update demo_main.py

--- examples/tutorial_api_python/demo_main.py
+++ examples/tutorial_api_python/demo_main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from sys import platform
+import numpy as np
 from imutils.video import FPS
 import cv2
 import time
@@ -44,7 +45,6 @@ class Main:
         start = time.time()
         video=None
         count=0
-        frame_num = 0
 
         try:
             # Starting OpenPose. Construct OpenPose object allocates GPU memory
@@ -55,6 +55,8 @@ class Main:
             datum = op.Datum()
 
             fps = self.stream.get(cv2.CAP_PROP_FPS)
+            framecount = self.stream.get(cv2.CAP_PROP_FRAME_COUNT)
+            print('Total frames in this video: ' + str(framecount))
             
             if (self.stream.isOpened() == False):
                 sys.exit("Error opening video stream or file")
@@ -82,13 +84,58 @@ class Main:
                         video = cv2.VideoWriter('output.avi', fourcc, fps, (width, height))
 
                     no_display = False
-                    cv2.imshow("OpenPose 1.7.0 - Tutorial Python API", datum.cvOutputData)
+
+                    # Start of Applications
+                    instances=len(datum.poseKeypoints)
+
+                    print("Number of instances per image:    ", instances)
+                    for i in range(0,instances):
+                        print("Instance : ", i)
+                        RbaseY = datum.poseKeypoints[i][4][1]  # 4: 'RWrist' 3: 'RElbow' (0,1,2) = (x,y,c)
+                        LbaseY = datum.poseKeypoints[i][7][1]  # 7: 'LWrist' 6: 'LElbow' (0,1,2) = (x,y,c)
+                        NoseY  = datum.poseKeypoints[i][0][1]  # 0: 'Nose'               (0,1,2) = (x,y,c)
+                        NoseX  = datum.poseKeypoints[i][0][0]  # 0: 'Nose'               (0,1,2) = (x,y,c)
+                        RAnkleY =datum.poseKeypoints[i][11][1] # 11:'RAnkle'             (0,1,2) = (x,y,c)
+                        RAnkleX =datum.poseKeypoints[i][11][0] # 11:'RAnkle'             (0,1,2) = (x,y,c)
+                        LAnkleY =datum.poseKeypoints[i][14][1] # 14:'LAnkle'             (0,1,2) = (x,y,c)
+                        LAnkleX =datum.poseKeypoints[i][14][0] # 14:'LAnkle'             (0,1,2) = (x,y,c)
+                        s11= (RAnkleX, RAnkleY)
+                        s14= (LAnkleX, LAnkleY)
+                        s0 = (NoseX, NoseY)
+                        barX = (RAnkleX + LAnkleX) * .5
+                        barY = (RAnkleY + LAnkleY) * .5
+                        # Fall
+                        # The angle between the centerline of the body and the ground
+                        theta= np.arctan(abs((NoseY-barY)/(NoseX-barX))) # angle in degree = angle in radian * 180/pi
+                        theta=theta*(180/np.pi)
+                        theta0=45
+                        '''
+                        print(s11, s14)
+                        print("NoseX = {:.1f}, NoseY = {:.1f}, barX = {:.1f}, barY = {:.1f}, A = {:.3f}".format(NoseX, NoseY, barX, barY, theta))
+                        if (NoseX==0.0 and NoseY==0.0):
+                            print("State : Not in ROI")
+                        elif (theta < theta0):
+                            print("State : Fall")
+                        else:
+                            print("State : Normal")
+                        '''
+                        # Hands Up
+                        print("NoseY = {:.1f}, RbaseY = {:.1f}, LbaseY = {:.1f}".format(NoseY, RbaseY, LbaseY))
+                        if (NoseY==0.0 or RbaseY==0.0 or LbaseY==0.0):
+                            print("State : Not in ROI")
+                        elif (RbaseY < NoseY or LbaseY < NoseY):
+                            print("State : Hands Up")
+                            cv2.putText(opframe, 'Hands Up', (30, 180),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                        else:
+                            print("State : Normal" + "\n")
+
+                    cv2.imshow("OpenPose 1.7.0 - Tutorial Python API", opframe)
+                    video.write(opframe)
+                    # End of Applications
 
                     key = cv2.waitKey(1)
                     if key & 0xFF == ord('q') or key == 27:
                         break
-                    
-                    video.write(opframe)
 
                 else:
                     break
@@ -99,6 +146,8 @@ class Main:
         except Exception as e:
             print(e)
             sys.exit(-1)
+
+        print("COMPLETED!!!")
 
 if __name__ == "__main__":
     
